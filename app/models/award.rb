dc624aaa899daf69139bda2b64a742bfee439eb3
Dalton Boll <daltonboll@gmail.com>
2015-12-10 04:00:04 -0800

    Add awards to database

--- app/models/award.rb
+++ app/models/award.rb
@@ -5,14 +5,70 @@
 #  id          :integer          not null, primary key
 #  title       :text
 #  event       :text
-#  dateAwarded :date
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  artist_id   :integer
 #  album_id    :integer
+#  yearAwarded :integer
 #
 
 class Award < ActiveRecord::Base
   belongs_to :artist # An Award belongs to an Artist
   belongs_to :album # An Award belongs to an Album
+
+  def self.populate_database
+    beginning_year = 1975
+    ending_year = 2014
+    award_events = []
+    award_titles = []
+
+    File.open("lib/award_events.txt", "r") do |f|
+      f.each_line do |event|
+        award_events.append(event.squish)
+      end
+    end
+
+    File.open("lib/award_titles.txt", "r") do |f|
+      f.each_line do |title|
+        award_titles.append(title.squish)
+      end
+    end
+
+    (beginning_year..ending_year).each do |year|
+      award_events.each do |event|
+        award_titles.each do |title|
+          award_hash = { title: title, event: event, yearAwarded: year }
+          award = Award.create(award_hash)
+          award.save
+        end
+      end
+    end
+  end
+
+  def self.assign_awards
+    artists = Artist.all
+    award_numbers = [0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    award_numbers_len = award_numbers.size - 1
+
+    artists.each do |artist|
+      num_artist_awards = award_numbers[Random.rand(0..award_numbers_len)]
+      earliest_award = artist.debutYear
+      awards = Award.where('"yearAwarded" >= ?', earliest_award)
+
+      if awards.nil? or awards.size == 0
+        next
+      end
+
+      num_awards = awards.size - 1
+
+      (0..num_artist_awards).each do |i|
+        award = awards[Random.rand(0..num_awards)]
+        if award.artist.nil?
+          artist.awards << award
+          artist.save
+        end
+      end
+    end
+  end
+
 end
