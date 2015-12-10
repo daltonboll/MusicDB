7ae8e308220ef7c0e71c7fe37da9241acf2d8615
Dalton Boll <daltonboll@gmail.com>
2015-12-09 18:45:38 -0800

    Annotate

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,19 +2,22 @@
 #
 # Table name: albums
 #
-#  id               :integer          not null, primary key
-#  title            :text
-#  releaseDate      :integer
-#  amountSold       :integer
-#  billboard200Peak :integer
-#  numberOfSingles  :integer
-#  created_at       :datetime         not null
-#  updated_at       :datetime         not null
-#  artist_id        :integer
-#  genres           :string           default([]), is an Array
-#  images           :string           default([]), is an Array
-#  popularity       :integer          default(0)
-#  spotifyID        :string
+#  id                   :integer          not null, primary key
+#  title                :text
+#  amountSold           :integer
+#  billboard200Peak     :integer
+#  numberOfSingles      :integer
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  artist_id            :integer
+#  genres               :string           default([]), is an Array
+#  images               :string           default([]), is an Array
+#  popularity           :integer          default(0)
+#  spotifyID            :string
+#  releaseDate          :string
+#  albumType            :string
+#  releaseDatePrecision :string
+#  spotifyArtistID      :string
 #
 
 class Album < ActiveRecord::Base
