93615cc9fb38e03ee98dce0de079f27f440645a4
Dalton Boll <daltonboll@gmail.com>
2015-12-09 17:03:12 -0800

    Add Spotify Artist population API calls

--- app/models/spotify_artist.rb
+++ app/models/spotify_artist.rb
@@ -13,4 +13,39 @@
 #
 
 class SpotifyArtist < ActiveRecord::Base
+  validates :name, :spotifyID, uniqueness: true
+  require 'rspotify'
+
+  def self.get_spotify_artists
+    RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+    artists = Artist.all
+
+    artists.each do |artist|
+      search_name = artist.name
+      puts "Getting info for artist: #{search_name}"
+      spotify_artists = RSpotify::Artist.search(search_name)
+      spotify_artist = spotify_artists.first
+
+      spotifyID = spotify_artist.id
+      popularity = spotify_artist.popularity
+      images_hash = spotify_artist.images
+      genres = spotify_artist.genres
+      name = spotify_artist.name
+
+      # Only grab the urls of the images
+      images = []
+      images_hash.each do |image_hash|
+        url = image_hash["url"]
+        images << url
+      end
+
+      artist_hash = { name: name, popularity: popularity, spotifyID: spotifyID, images: images, genres: genres }
+      new_artist = SpotifyArtist.create(artist_hash)
+      new_artist.save
+      sleep(1)
+    end
+
+    return true
+  end
+
 end
